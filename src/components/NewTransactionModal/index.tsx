1cdb7c2eb4ab2003c8f21876c926b094999f5945
Victor Pinheiro <victorca2011@gmail.com>
2024-08-30 11:58:18 -0300

    dt money

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,100 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+import * as z from 'zod'
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { useContextSelector } from 'use-context-selector';
+
+const NewTransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    type: z.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>;
+
+export function NewTransactionModal() {
+    const createTransaction = useContextSelector(TransactionsContext, 
+        (context) => {
+        return context.createTransaction;
+    })
+
+    const {
+        control,
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+        reset,
+
+    } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(NewTransactionFormSchema)
+    })
+
+    async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        const { description, price, category, type } = data;
+
+        await createTransaction({
+            description,
+            price,
+            category,
+            type,
+        });
+
+        reset();
+    }
+
+    return (
+        <Dialog.Portal>
+            <Overlay />
+            <Content>
+                <Dialog.Title>
+                    Nova Transação
+                </Dialog.Title>
+                <CloseButton>
+                    <X size={24} />
+                </CloseButton>
+                <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                    <input
+                        type="text" placeholder='Descrição' required
+                        {...register('description')}
+                    />
+                    <input
+                        type="number" placeholder='Preço' required
+                        {...register('price', { valueAsNumber: true })}
+                    />
+                    <input
+                        type="text" placeholder='Categoria' required
+                        {...register('category')}
+                    />
+
+                    <Controller
+                        control={control}
+                        name="type"
+                        render={({ field }) => {
+                            return (
+                                <TransactionType
+                                    onValueChange={field.onChange}
+                                    value={field.value}>
+                                    <TransactionTypeButton variant='income' value='income'>
+                                        <ArrowCircleUp size={24} />
+                                        Entrada
+                                    </TransactionTypeButton>
+                                    <TransactionTypeButton variant='outcome' value='outcome'>
+                                        <ArrowCircleDown size={24} />
+                                        Saída
+                                    </TransactionTypeButton>
+                                </TransactionType>
+                            )
+                        }} />
+
+                    <button type="submit" disabled={isSubmitting}> Cadastrar </button>
+                </form>
+
+            </Content>
+        </Dialog.Portal>
+    )
+}
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,44 @@
+
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+
+import { PriceHighLigh, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
+import { useContextSelector } from "use-context-selector";
+
+export function Transactions() {
+    const transactions = useContextSelector(TransactionsContext, (context) => {
+        return context.transactions;
+    })
+
+    return (
+        <div>
+            <Header />
+            <Summary />
+            <TransactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td width="50%">
+                                        <PriceHighLigh variant={transaction.type}>
+                                            {transaction.type == 'outcome' && '- '}
+                                            {priceFormatter.format(transaction.price)}
+                                        </PriceHighLigh>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+        </div>
+    )
+}
